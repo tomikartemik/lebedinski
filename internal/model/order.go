dca93b1060a22f0762caba4739c181f7423dd410
tomikartemik <artemules@mail.ru>
2025-03-30 17:02:30 +0300

    UPD: cart added

--- internal/model/order.go
+++ internal/model/order.go
@@ -3,10 +3,11 @@ package model
 import "time"
 
 type Order struct {
-	ID         int        `gorm:"primaryKey;autoIncrement" json:"id"`
-	FullName   string     `json:"full_name"`
-	Phone      string     `json:"phone"`
-	Telegram   string     `json:"telegram"`
-	CreatedAt  time.Time  `json:"created_at"`
-	OrderItems []CartItem `gorm:"foreignKey:OrderID" json:"order_items"` // Товары в заказе
+	ID        int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	FullName  string    `json:"full_name"`
+	Phone     string    `json:"phone"`
+	Telegram  string    `json:"telegram"`
+	CreatedAt time.Time `json:"created_at"`
+	CartID    int       `gorm:"not null" json:"cart_id"`
+	Cart      Cart      `gorm:"foreignKey:CartID" json:"cart"`
 }
